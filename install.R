cb16ff9edeba596ceacc28b57d6627a1a8bbed80
lgnbhl <felix.luginbuhl@protonmail.ch>
2020-01-24 02:31:13 +0100

    add install.R

---
+++ install.R
@@ -0,0 +1,19 @@
+# Install all packages used in my blog posts
+
+# install/load needed R packages
+if (!require("pacman")) install.packages("pacman")
+
+pacman::p_load(tidyverse, ggalt, ggiraph, ggmap, ggthemes, maps)
+pacman::p_load(tidyverse, DT, ggthemes, wordcloud, SnowballC, tidytext, tm, caret, h2o, rpart.plot, rsample, xgboost)
+pacman::p_load(tidyverse, lubridate, dygraphs, hrbrthemes, forecast, scales, sweep, timetk)
+pacman::p_load(tidyverse, rvest, janitor, WikipediR, tidytext, countrycode, leaflet, rnaturalearth, rnaturalearthdata, htmltools)
+pacman::p_load(tidyverse, rvest, sf, countrycode, ggalt, ggthemes, hrbrthemes, leaflet, RColorBrewer, readxl, rnaturalearth, rnaturalearthdata, rgeos)
+pacman::p_load(tidyverse, lubridate, scales, waffle)
+pacman::p_load(Amelia, caret, e1071, hrbrthemes, rvest, tidytext, tidyverse, tm, xgboost)
+pacman::p_load(tidyverse, rvest, tidygraph, ggraph, ggforce, visNetwork)
+pacman::p_load(broom, gvlma, countrycode, ggimage, ggthemes, plotly, pscl, readxl, reshape2, rvest, tidyverse)
+pacman::p_load(tidyverse, ggthemes, scales, tidyr, tidytext, wordcloud)
+pacman::p_load(tidyverse, DT, treemap, sunburstR, d3r, htmlwidgets)
+pacman::p_load(tidyverse, sf, tmap, srvyr, plotly, gganimate, quantreg, countrycode, hrbrthemes, knitr)
+pacman::p_load(tidyverse, scales, colorspace, plotly, RSwissMaps, BFS)
+pacman::p_load(tidyverse, dygraphs, ggthemes, lubridate, magick, RColorBrewer, rtweet, httpuv, scales, tidytext, xts)
